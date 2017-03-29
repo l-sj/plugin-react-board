ec978b63f844cb7365fc9bf90241d1341525ed91
akasima <akasima@nate.com>
2017-03-29 11:13:03 +0900

    container apis url decode

--- views/defaultSkin/container.blade.php
+++ views/defaultSkin/container.blade.php
@@ -18,21 +18,21 @@
 
             //API 정보
             apis: {
-                show: '{{instanceRoute('api.show', ['id' => '[id]'], $instanceId)}}',
-                list: '{{instanceRoute('api.list', [], $instanceId)}}',
-                store: '{{instanceRoute('api.store', [], $instanceId)}}',
-                update: '{{instanceRoute('api.update', ['id' => '[id]'], $instanceId)}}',
-                destroy: '{{instanceRoute('api.destroy', ['id' => '[id]'], $instanceId)}}',
-                category: '{{instanceRoute('api.category', [], $instanceId)}}',
+                show: '{{urldecode(instanceRoute('api.show', ['id' => '[id]'], $instanceId))}}',
+                list: '{{urldecode(instanceRoute('api.list', [], $instanceId))}}',
+                store: '{{urldecode(instanceRoute('api.store', [], $instanceId))}}',
+                update: '{{urldecode(instanceRoute('api.update', ['id' => '[id]'], $instanceId))}}',
+                destroy: '{{urldecode(instanceRoute('api.destroy', ['id' => '[id]'], $instanceId))}}',
+                category: '{{urldecode(instanceRoute('api.category', [], $instanceId))}}',
                 /** 어떻게 사용하는건지? */
-                favorite_create: '{{instanceRoute('api.favorite.create', ['id' => '[id]'], $instanceId)}}',
-                favorite_destroy: '{{instanceRoute('api.favorite.create', ['id' => '[id]'], $instanceId)}}',
+                favorite_create: '{{urldecode(instanceRoute('api.favorite.create', ['id' => '[id]'], $instanceId))}}',
+                favorite_destroy: '{{urldecode(instanceRoute('api.favorite.create', ['id' => '[id]'], $instanceId))}}',
                 temp: ''
             },
 
             //링크 정보
             links: {
-                settings: '{{route('settings.react_board.edit', ['boardId' => $instanceId])}}'
+                settings: '{{urldecode(route('settings.react_board.edit', ['boardId' => $instanceId]))}}'
             },
             ajaxHeaders: {
                 'X-CSRF-TOKEN': '{!! csrf_token() !!}'
